e669fe7696d1a63790451242e09d065b5d170220
dzgeorgy <dzgeorgy@gmail.com>
2020-02-08 17:52:35 +0300

    Initial commit

---
+++ customize.sh
@@ -0,0 +1,12 @@
+ui_print "****************************************************"
+ui_print "          OnePlus 2 Navigation Bar Enabler          "
+ui_print "                    by @dzgeorgy                    "
+ui_print "****************************************************"
+
+ui_print "Installing Module!"
+
+DEVICE=`getprop ro.product.device`
+RELEASE=`getprop ro.build.version.release`
+
+ui_print "Device: "$DEVICE
+ui_print "Android Version: "$RELEASE
